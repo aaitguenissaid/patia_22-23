9557ed24d45ead8ee7f5752ccb9d244bade29f13
AbderrahmaneAit gueni ssaid <aaitguenissaid@gmail.com>
2023-03-24 10:27:04 +0100

    first commit for SAT solver

---
+++ SAT/scripts/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ..
+javac -d classes -cp lib/pddl4j-4.0.0.jar src/fr/uga/pddl4j/examples/sat/SAT.java
---
+++ SAT/scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ..
+java -cp classes:lib/pddl4j-4.0.0.jar fr.uga.pddl4j.examples.sat.SAT --help
