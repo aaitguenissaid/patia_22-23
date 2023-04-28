42acee380ff5f633f20ccad6e1fc4a0ed96e7c47
AbderrahmaneAit gueni ssaid <aaitguenissaid@gmail.com>
2023-04-28 09:40:13 +0200

    clean git

--- SAT/scripts/run_depot_pddl4j.sh
+++ SAT/scripts/run_depot_pddl4j.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 path="resources/depots_ipc2002_strips-automatic/"
-java -cp lib/pddl4j-4.0.0.jar fr.uga.pddl4j.planners.statespace.HSP $path"domain.pddl" $path"p01.pddl"
+java -cp lib/pddl4j-4.0.0.jar fr.uga.pddl4j.planners.statespace.HSP $path"domain.pddl" $path"p01.pddl"  -t 600
 
