d49e722ad7b35ac85f9e9f024a54d95952c1f9cf
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-28 11:19:18 +0200

    trash files removed

--- sokoban/parsers/src/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        SokobanDomainParser sokobanDomainParser = new SokobanDomainParser();
-        sokobanDomainParser.parse("src/main/resources/test00.json");
-        System.out.println("Done");
-    }
-}
